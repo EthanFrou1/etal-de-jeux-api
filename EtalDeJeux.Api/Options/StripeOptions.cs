80a87914cf68cf9bf5f14e54079837e360f6f7c9
EthanFrou1 <ethanfrou1@gmail.com>
2025-10-15 15:43:51 +0200

    Verify Stripe account before checkout

--- EtalDeJeux.Api/Options/StripeOptions.cs
+++ EtalDeJeux.Api/Options/StripeOptions.cs
@@ -5,4 +5,5 @@ public class StripeOptions
     public string? SecretKey { get; set; }
     public string? PublishableKey { get; set; }
     public string? WebhookSecret { get; set; }
+    public string? ExpectedAccountId { get; set; }
 }
